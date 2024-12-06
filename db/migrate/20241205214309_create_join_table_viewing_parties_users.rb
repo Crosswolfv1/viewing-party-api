3cacbb34d2324061cd2e3bd8e196c1c97a54ce50
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-12-06 15:12:55 -0600

    added faker

--- db/migrate/20241205214309_create_join_table_viewing_parties_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableViewingPartiesUsers < ActiveRecord::Migration[7.1]
-  def change
-    create_join_table :viewing_parties, :users do |t|
-      # t.index [:viewing_party_id, :user_id]
-      # t.index [:user_id, :viewing_party_id]
-    end
-  end
-end
