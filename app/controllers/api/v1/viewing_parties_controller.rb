42dcf5b8e348bdbd5cabe2f0aae7fa3e5001cfe8
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-12-08 14:31:49 -0600

    Added validations for sad paths

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -1,13 +1,16 @@
 class Api::V1::ViewingPartiesController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
   rescue_from ArgumentError, with: :invalid_parameters
   def create
     validate_params(params)
-    new = ViewingParty.create(viewing_party_params)
+    new_party = ViewingParty.create!(viewing_party_params)
+    render json: ViewingPartySerializer.new(new_party, { params: { invitees: params[:invitees] } }), status: :created
     user = User.find(params[:user_id])
-    UsersViewingParty.create( { viewing_party_id: new.id, user_id: user.id, host: true} )
+    UsersViewingParty.create!( { viewing_party_id: new_party.id, user_id: user.id, host: true} )
+
     params[:invitees].each do |invitee_id|
       invitee = User.find(invitee_id)
-      UsersViewingParty.create({ viewing_party_id: new.id, user_id: invitee.id, host: false})
+      UsersViewingParty.create!({ viewing_party_id: new_party.id, user_id: invitee.id, host: false})
     end
   end
 
@@ -18,54 +21,41 @@ class Api::V1::ViewingPartiesController < ApplicationController
   end
 
   def validate_params(params)
+    raise ArgumentError, "Validation failed: Invitees can't be blank" unless params.has_key?(:invitees)
     validate_user(params[:user_id])
     validate_invitees(params[:invitees])
+    validate_start_end_time(params)
+    validate_session_length(params)
+  end
+
+  def validate_session_length(params)
+    start_time = Time.parse(params[:start_time]).to_i
+    end_time = Time.parse(params[:end_time]).to_i
+    binding.pry
   end
 
   def validate_user(user_id)
     raise ArgumentError, "User_id is invalid" unless User.find_by(id: user_id)
   end
 
+  def validate_start_end_time(params)
+    raise ArgumentError, "Invalid or missing start_time or end_time" if params[:start_time].nil? || params[:end_time].nil?
+    start_time = Time.parse(params[:start_time])
+    end_time = Time.parse(params[:end_time])
+    raise ArgumentError, "End time cannot be before start time" unless start_time < end_time
+  end
+
   def validate_invitees(invitees)
     invitees.each do |invitee|
-      raise ArgumentError, "Invitee is invalid" unless User.find_by(id: invitee)
+      raise ArgumentError, "Invitee:#{invitee} is invalid" unless User.find_by(id: invitee)
     end
   end
 
   def invalid_parameters(exception)
     render json: ErrorSerializer.format_error(ErrorMessage.new(exception, 400)), status: :bad_request
   end
+
+  def record_invalid(exception)
+    render json: ErrorSerializer.format_error(ErrorMessage.new(exception, 400)), status: :bad_request
+  end
 end
-
-
-
-# {
-#   "data": {
-#     "id": "1",
-#     "type": "viewing_party",
-#     "attributes": {
-#       "name": "Juliet's Bday Movie Bash!",
-#       "start_time": "2025-02-01 10:00:00",
-#       "end_time": "2025-02-01 14:30:00",
-#       "movie_id": 278,
-#       "movie_title": "The Shawshank Redemption",
-#       "invitees": [
-#         {
-#           "id": 11,
-#           "name": "Barbara",
-#           "username": "leo_fan"
-#         },
-#                 {
-#           "id": 7,
-#           "name": "Ceci",
-#           "username": "titanic_forever"
-#         },
-#                 {
-#           "id": 5,
-#           "name": "Peyton",
-#           "username": "star_wars_geek_8"
-#         }
-#       ]
-#     }
-#   }
-# }
