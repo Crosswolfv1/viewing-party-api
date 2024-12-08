18c2e7295b79080ef93e6faabb199f2dbc4eddf2
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-12-08 16:40:25 -0600

    fixed tests broken by fixing the typo

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe ViewingParty, type: :model do
   describe "validations" do
-    it { should have_many :users_viewing_parties }
-    it { should have_many(:users).through(:users_viewing_parties) }
+    it { should have_many :user_viewing_parties }
+    it { should have_many(:users).through(:user_viewing_parties) }
 
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:start_time) }
