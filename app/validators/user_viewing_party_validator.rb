ca85baa83c48412a932b33db230465527420ac3f
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-12-08 17:55:04 -0600

    fixed validators added invitee validation

--- app/validators/user_viewing_party_validator.rb
+++ app/validators/user_viewing_party_validator.rb
@@ -6,16 +6,21 @@ class UserViewingPartyValidator
   def validate_all
     validate_party
     validate_user
+    validate_invitees
   end
 
 
   private
 
   def validate_party
-    raise ArgumentError, "Validation failed: Invalid Party ID" unless ViewingParty.find(@params[:party_id]).present?
+    raise ArgumentError, "Validation failed: Invalid Party ID" unless ViewingParty.find_by(id: @params[:party_id]).present?
   end
 
   def validate_user
-    raise ArgumentError, "Validation failed: Invalid User ID" unless User.find(@params[:invitees_user_id]).present?
+    raise ArgumentError, "Validation failed: Invalid User ID" unless User.find_by(id: @params[:viewing_party_user_id]).present?
+  end
+
+  def validate_invitees
+    raise ArgumentError, "Validation failed: Invalid Invitee ID" unless User.find_by(id: @params[:invitees_user_id]).present?
   end
 end
