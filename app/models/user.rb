f400f47868a55a5e08fc6193dfaeed64d5f00861
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-12-08 23:23:09 -0600

    created instance methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,36 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :api_key
 
+  def hosted_viewing_party
+    ViewingParty.joins(:user_viewing_parties
+    ).where(user_viewing_parties: {user_id: id, host: true}
+    ).map do |party|
+      {
+        id: party[:id],
+        name: party[:name],
+        start_time: party[:start_time],
+        end_time: party[:end_time],
+        movie_id: party[:movie_id],
+        movie_title: party[:movie_title],
+        host_id: id
+      }
+    end
+  end
+
+  def invited_viewing_party
+    ViewingParty.joins(:user_viewing_parties
+    ).where(user_viewing_parties: {user_id: id, host: false}
+    ).map do |party|
+      {
+        id: party[:id],
+        name: party[:name],
+        start_time: party[:start_time],
+        end_time: party[:end_time],
+        movie_id: party[:movie_id],
+        movie_title: party[:movie_title],
+        host_id: UserViewingParty.where(viewing_party_id: party[:id], host: true).pluck(:user_id)
+      }
+    end
+
+  end
 end
